5cdc02e582f843dfea604ef8c17774529edcc527
Milo Wyner <milowyner@gmail.com>
2021-09-10 14:30:35 -0700

    Create basic UI that's a list of photos and names of people with an Add button

--- NamesToFaces/ContentView.swift
+++ NamesToFaces/ContentView.swift
@@ -9,8 +9,23 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List(0..<3) { _ in
+                HStack {
+                    Image("TestPhoto")
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 60, height: 60)
+                        .clipShape(RoundedRectangle(cornerRadius: 10))
+                    Text("First Last")
+                        .font(.title3)
+                }
+            }
+            .navigationTitle(Text("Names to Faces"))
+            .navigationBarItems(trailing: Button("Add") {
+                // open image picker
+            })
+        }
     }
 }
 
